e175fea7a44ba234eecea799e38e4505c9edc17e
yehwansong <yehwanyensong@gmail.com>
2019-12-08 19:36:50 +0900

    add

---
+++ script.js
@@ -0,0 +1,10 @@
+$( document ).ready(function() {
+	// getmain()
+	// function getmain(){
+	// 	if($('#main').length>0){
+	// 		var zoom = $('#main').clone().appendTo("#wrap");
+	// 	}else{
+	// 		setTimeout(function(){getmain()}, 500);
+	// 	}
+	// }
+});
